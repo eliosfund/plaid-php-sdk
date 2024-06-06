39b929416c94c2ba88bff85624658846392fb764
Jon Erickson <jon.erickson@eliosfund.com>
2024-06-05 21:11:18 -0700

    Add tests

---
+++ tests/Feature/PlaidTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Plaid\Plaid;
+use Saloon\Http\Faking\MockResponse;
+
+test('it will resolve the default base url', function () {
+    $connection = new Plaid('foo', 'bar', 'foobar');
+
+    expect($connection->resolveBaseUrl())->toEqual('https://production.plaid.com');
+});
+
+test('it will resolve the sandbox url', function () {
+    $connection = new Plaid('foo', 'bar', 'foobar', 'staging');
+
+    expect($connection->resolveBaseUrl())->toEqual('https://sandbox.plaid.com');
+});
+
+test('it will resolve the development url', function () {
+    $connection = new Plaid('foo', 'bar', 'foobar', 'development');
+
+    expect($connection->resolveBaseUrl())->toEqual('https://development.plaid.com');
+});
+
+test('it will set the default body', function () {
+    $connection = new Plaid('foo', 'bar', 'foobar');
+
+    expect($connection->body()->all())
+        ->toBeArray()
+        ->toMatchArray([
+            'client_id' => 'foo',
+            'secret' => 'bar',
+            'access_token' => 'foobar',
+        ]);
+});
+
+test('it will set the correct headers', function () {
+    $connection = new Plaid('foo', 'bar', 'foobar');
+
+    expect($connection->headers()->all())
+        ->toBeArray()
+        ->toMatchArray([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ]);
+});
